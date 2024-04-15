b8356f144170aa936b256c81da58daa6eb8ca9c4
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-15 18:26:36 +0200

    Project Created

---
+++ TheYodaAI-AppSln/TheYodaAI-App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TheYodaAI_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
