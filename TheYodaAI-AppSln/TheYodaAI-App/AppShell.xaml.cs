74aba02b24350518d2e25e17fb4e5998b6f6ab72
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-16 16:45:55 +0200

    Fun Facts Pages & Routes done

--- TheYodaAI-AppSln/TheYodaAI-App/AppShell.xaml.cs
+++ TheYodaAI-AppSln/TheYodaAI-App/AppShell.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace TheYodaAI_App
+﻿using TheYodaAI_App.Views;
+
+namespace TheYodaAI_App
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            RegisterRoutes();
+        }
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute("funfactspage", typeof(FunFactsPage));
         }
     }
 }
---
+++ TheYodaAI-AppSln/TheYodaAI-App/Views/FunFactsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TheYodaAI_App.Views;
+
+public partial class FunFactsPage : ContentPage
+{
+	public FunFactsPage()
+	{
+		InitializeComponent();
+	}
+}
