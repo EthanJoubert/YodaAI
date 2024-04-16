967ab5380e812b282d733f488135dacd5859e8d6
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-16 17:48:43 +0200

    Simple Ui Created

--- TheYodaAI-AppSln/TheYodaAI-App/ViewModels/FunFactPageViewModel.cs
+++ TheYodaAI-AppSln/TheYodaAI-App/ViewModels/FunFactPageViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheYodaAI_App.Views;
 
 namespace TheYodaAI_App.ViewModels
 {
     public class FunFactPageViewModel : BaseViewModel
     {
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
