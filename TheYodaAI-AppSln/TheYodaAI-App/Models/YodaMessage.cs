e23bfbea171651422ec50e0a4b52931c003b4c4f
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-17 15:38:16 +0200

    Enums created (App still works)

---
+++ TheYodaAI-AppSln/TheYodaAI-App/Models/YodaMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheYodaAI_App.Enums;
+
+namespace TheYodaAI_App.Models
+{
+    public class YodaMessage
+    {
+        public YodaMessageTypeEnums MessageType { get; set; }
+        public string? MessageBody { get; set; }
+    }
+}
