633b408486836221e6a4b9631de392b4ed6cfd9a
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-17 16:31:35 +0200

    Trying to set up yoda (App still runs here)

--- TheYodaAI-AppSln/TheYodaAI-App/Services/YodaAssistant.cs
+++ TheYodaAI-AppSln/TheYodaAI-App/Services/YodaAssistant.cs
@@ -1,4 +1,5 @@
-﻿using Azure.AI.OpenAI;
+﻿using Azure;
+using Azure.AI.OpenAI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,7 +37,26 @@ namespace TheYodaAI_App.Services
 
         public ChatResponseMessage GetCompletion(IList<YodaMessage> chatInboundHistory, YodaMessage userMessage)
         {
-            throw new NotImplementedException();
+            var messages = BuildChatContext(chatInboundHistory, userMessage);
+
+            var client = new OpenAIClient(new Uri(_settings.AzureOpenAiEndPoint), new AzureKeyCredential(_settings.AzureOpenAiKey));
+            string deploymentName = "gpt35turbo16";
+            string searchIndex = "index";
+
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+
+
+            };
+            foreach (var message in messages)
+            {
+                chatCompletionsOptions.Messages.Add(message);
+            }
+            Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
+            ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
+
+            return responseMessage;
+            //throw new NotImplementedException();
         }
     }
 }
