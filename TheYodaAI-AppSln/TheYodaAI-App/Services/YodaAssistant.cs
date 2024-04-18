7eb649c59113ff81a989cd118eabadedddf7bee8
EthanJoubert <124621733+EthanJoubert@users.noreply.github.com>
2024-04-18 11:56:54 +0200

    Update YodaAssistant (App still runs)

--- TheYodaAI-AppSln/TheYodaAI-App/Services/YodaAssistant.cs
+++ TheYodaAI-AppSln/TheYodaAI-App/Services/YodaAssistant.cs
@@ -20,27 +20,9 @@ namespace TheYodaAI_App.Services
             _settings = settings;
         }
 
-        // Do we Need Build Context???
-        private IList<ChatRequestMessage> BuildChatContext(IList<YodaMessage> chatInboundHistory, YodaMessage userMessage)
-        {
-            var chatContext = new List<ChatRequestMessage>();
-
-            chatContext.Add(new ChatRequestSystemMessage(YodaBehaviorDescription));
-
-            foreach (var chatMessage in chatInboundHistory)
-                chatContext.Add(new ChatRequestAssistantMessage(chatMessage.MessageBody));
-
-            chatContext.Add(new ChatRequestUserMessage(userMessage.MessageBody));
-
-            return chatContext;
-
-        }
-
         // Does Code from OpenAi portal go here??? 
         public ChatResponseMessage GetCompletion(IList<YodaMessage> chatInboundHistory, YodaMessage userMessage)
         {
-            var messages = BuildChatContext(chatInboundHistory, userMessage);
-
             var client = new OpenAIClient(new Uri(_settings.AzureOpenAiEndPoint), new AzureKeyCredential(_settings.AzureOpenAiKey));
             string deploymentName = "gpt35turbo16";
             //string searchIndex = "index";
@@ -50,15 +32,10 @@ namespace TheYodaAI_App.Services
                 
 
             };
-            foreach (var message in messages)
-            {
-                chatCompletionsOptions.Messages.Add(message);
-            }
             Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
             ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
 
             return responseMessage;
-            //throw new NotImplementedException();
         }
     }
 }
